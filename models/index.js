ad13fa08574df09606cbc449a48fe1f8996b9c31
Jrsparl <john@sparlingmusic.com>
2021-01-29 00:44:38 -0500

    moved code from module, edited and new style

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+  hooks: true,
+});
+
+module.exports = { User, Post, Comment };
